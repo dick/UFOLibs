8ce6e47e5dbc5f80e34c19e2fb1132e0f0892072
Dick Hall <dickhall@gmail.com>
2011-04-16 17:26:01 -0500

    fixed query - strings

--- main.rb
+++ main.rb
@@ -19,8 +19,9 @@ class Main < Sinatra::Base
 	helpers do
 
 		def ufo_search(description, location, size)
-			results = Chimps::QueryRequest.new("science/astronomy/seti/nuforc/ufo_sightings_search", :query_params => { "description" => description, "location" => location, "size" => size } ).get
-			return results.parse!.body	
+			results = Chimps::QueryRequest.new("science/astronomy/seti/nuforc/ufo_sightings_search", :query_params => { "description" => description, "location" => location, "size" => size } )
+			p results
+			return results.get.parse!.body	
 		end
 
 	end
