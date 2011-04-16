65a1a66219e8c35f0adcb748069d40c84ca0691d
Dick Hall <dickhall@gmail.com>
2011-04-16 17:56:02 -0500

    fixed query - strings

--- main.rb
+++ main.rb
@@ -32,7 +32,7 @@ class Main < Sinatra::Base
   end
 
 	get '/find/:verb/:location' do
-		@results = ufo_search(params[:verb], params[:location], 5)
+		@results = ufo_search(params[:verb].to_s, params[:location].to_s, 5.to_s)
 	  @title = "#{params[:verb]}/#{params[:location]}"
 		haml :display_results
 	end
